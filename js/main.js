bee3393f243476adf36639497a91796697030079
Zachary Robbins <xaqiri@outlook.com>
2017-03-18 14:04:09 -0500

    Added full functionality
    All buttons work appropriately now

---
+++ js/main.js
@@ -0,0 +1,95 @@
+let display = document.querySelector('#display')
+let clear = document.querySelector('#clear')
+let clearEntry = document.querySelector('#clearEntry')
+let backspace = document.querySelector('#backspace')
+let negation = document.querySelector('#negation')
+let decimal = document.querySelector('#decimal')
+let addition = document.querySelector('#addition')
+let subtraction = document.querySelector('#subtraction')
+let multiplication = document.querySelector('#multiplication')
+let division = document.querySelector('#division')
+let equals = document.querySelector('#equals')
+
+let sum = []
+let entry = []
+let keypad = []
+let operators = {
+	addition: '+',
+	subtraction: '-',
+	multiplication: '*',
+	division: '/'
+}
+
+const operation = (operator) => {
+	if (entry.length > 0) {
+		sum.push(entry.join(''))
+		sum.push(operator)
+		display.textContent = entry.join('')
+		entry = []
+	}
+}
+
+for (let i = 0; i < 10; i++) {
+	keypad.push(document.querySelector('#numpad'+i))
+	keypad[i].addEventListener('click', function () {
+		if (!(i === 0 && entry.length === 0)) {
+			entry.push(this.textContent)
+			display.textContent = entry.join('')
+		}
+	})
+}
+
+clear.addEventListener('click', function () {
+	entry = []
+	sum = []
+	display.textContent = 0
+})
+
+clearEntry.addEventListener('click', function () {
+	entry = []
+	display.textContent = 0
+})
+
+backspace.addEventListener('click', function () {
+	entry.pop()
+	if (entry.length > 0) display.textContent = entry.join('')
+	else display.textContent = 0
+})
+
+negation.addEventListener('click', function () {
+	if (entry.length != 0) {
+		entry.unshift('-')
+		display.textContent = entry.join('')
+	}
+})
+
+decimal.addEventListener('click', function () {
+	entry.push('.')
+	display.textContent = entry.join('')
+})
+
+addition.addEventListener('click', function () {
+	operation(operators.addition)
+})
+
+subtraction.addEventListener('click', function () {
+	operation(operators.subtraction)
+})
+
+division.addEventListener('click', function () {
+	operation(operators.division)
+})
+
+multiplication.addEventListener('click', function () {
+	operation(operators.multiplication)
+})
+
+equals.addEventListener('click', function () {
+	if (entry.length > 0) {
+		sum.push(entry.join(''))
+		var total = eval(sum.join(''))
+		display.textContent = total
+		sum = []
+		entry = [total]
+	}
+})
